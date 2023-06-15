947335f8614b92b6b4c0ae1c534a115f1057fd87
j0lama <jon_larrea@hotmail.com>
2023-06-15 14:46:42 +0100

    FIFO max priority configured without kernel modifications

--- timechecker.c
+++ timechecker.c
@@ -1,9 +1,15 @@
 #include <linux/delay.h> /* usleep_range */
 #include <linux/kernel.h>
 #include <linux/kthread.h>
+#include <linux/sched.h>
 #include <linux/module.h>
 
 #define SLEEP_DELAY 1000 /* usec */
+
+/* From: https://elixir.bootlin.com/linux/v5.9.2/source/include/uapi/linux/sched/types.h#L7 */
+struct sched_param {
+	int sched_priority;
+};
 
 
 /* Globals */
@@ -54,11 +60,21 @@ static int timechecker_thread(void *data)
 
 static int __exit  timechecker_init(void)
 {
+    struct sched_param sp;
+
     printk(KERN_INFO "timechecker init: cpu_id(%d), threshold(%d)\n", cpu_id, threshold);
     kthread = kthread_create(timechecker_thread, NULL, "mykthread");
+
     /* Pin kthread to the specified CPU */
     kthread_bind(kthread, cpu_id);
+
+    /* Asign max FIFO priority */
+    sp.sched_priority = MAX_RT_PRIO;
+    sched_setscheduler_nocheck(kthread, SCHED_FIFO, &sp);
+
+    /* Start kthread */
     wake_up_process(kthread);
+
     return 0;
 }
 
